1acae0041b96214897b9806745edd7aee835c466
Manish Solanki <mk394@MKSolanki>
2026-01-12 17:08:29 +0530

    changes in conf

--- test/pageobjects/Android/login.Screen.ts
+++ test/pageobjects/Android/login.Screen.ts
@@ -9,20 +9,23 @@ class LoginPage {
     get password() {
         return $('~test-Password');
     }
-   
+
 
     async tapLogin() {
+        await this.loginButton.waitForDisplayed({ timeout: 30000 });
         await this.loginButton.click();
     }
-     async fillUsername() {
+    async fillUsername() {
+        await this.userName.waitForDisplayed({ timeout: 30000 });
         await this.userName.click();
         await this.userName.setValue("standard_user")
     }
-     async fillPassword() {
+    async fillPassword() {
+        await this.password.waitForDisplayed({ timeout: 30000 });
         await this.password.click();
         await this.password.setValue("secret_sauce")
     }
- 
+
 }
 
 export default new LoginPage();
