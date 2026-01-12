308f62827925c2e62615c86312c957895dfb59ff
Manish Solanki <mk394@MKSolanki>
2026-01-12 15:16:29 +0530

    changes in conf

--- wdio.conf.ts
+++ wdio.conf.ts
@@ -7,31 +7,44 @@ export const config: WebdriverIO.Config = {
     services: ['appium'],
 
 
+    // capabilities: [{
+
+
+    //     platformName: 'Android',
+    //     // 'appium:deviceName': '98cefeb9 ',
+    //     'appium:deviceName': 'emulator-5554',
+    //     'appium:automationName': 'UiAutomator2',
+
+    //     'appium:appPackage': 'com.swaglabsmobileapp',
+    //     'appium:appActivity': 'com.swaglabsmobileapp.SplashActivity',
+
+    //     // 🔑 APK PATH
+    //     'appium:app': 'apps/SauceLabs.app.2.7.1.apk',
+    //     // 🔑 FIX
+    //     "appium:ignoreHiddenApiPolicyError": true,
+
+
+    //     'appium:noReset': false,   // reinstall app every run
+    //     'appium:fullReset': true,
+
+    // }],
     capabilities: [{
-   
 
         platformName: 'Android',
-        // 'appium:deviceName': '98cefeb9 ',
         'appium:deviceName': 'emulator-5554',
-        'appium:automationName': 'UiAutomator2',
+        "appium:automationName": 'UiAutomator2',
+        "appium:app": './apps/Android.SauceLabs.apk',
 
-        'appium:appPackage': 'com.swaglabsmobileapp',
-        'appium:appActivity': 'com.swaglabsmobileapp.SplashActivity',
+        // Increase timeouts
+        "appium:adbExecTimeout": 60000,               // 60s for ADB commands like install/uninstall
+        "appium:uiautomator2ServerInstallTimeout": 120000,  // 120s for UiAutomator2 server install
+        "appium:newCommandTimeout": 300,
 
-        // 🔑 APK PATH
-        'appium:app': 'apps/SauceLabs.app.2.7.1.apk',
-        // 🔑 FIX
-        "appium:ignoreHiddenApiPolicyError": true,
-
-
-        'appium:noReset': false,   // reinstall app every run
-        'appium:fullReset': true,
-        
     }],
     mochaOpts: {
         timeout: 90 * 1000 // 60 seconds
     },
-    
+
     reporters: ['spec',
         ['allure', {
             outputDir: 'allure-results',
